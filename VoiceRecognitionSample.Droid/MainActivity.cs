a2af17ab717531f20b435d8b749df6c67c406382
達哉天谷 <amagai.tohoku@gmail.com>
2017-04-07 02:10:55 +0900

    Android側の修正ミスを修正

--- VoiceRecognitionSample.Droid/MainActivity.cs
+++ VoiceRecognitionSample.Droid/MainActivity.cs
@@ -4,25 +4,28 @@ using Android.Content.PM;
 using Android.OS;
 using Microsoft.Practices.Unity;
 using Prism.Unity;
-
-// ↓追加
 using Android.Preferences;
+using Android.Content;
 
 namespace VoiceRecognitionSample.Droid
 {
 	[Activity(Label = "VoiceRecognitionSample.Droid", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
 	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
 	{
-		// ↓追加
 		public event EventHandler<PreferenceManager.ActivityResultEventArgs> ActivityResult = delegate {};
 
 		protected override void OnCreate(Bundle bundle)
 		{
 			base.OnCreate(bundle);
-
 			global::Xamarin.Forms.Forms.Init(this, bundle);
-
 			LoadApplication(new App(new AndroidInitializer()));
+		}
+
+		protected override void OnActivityResult(int requestCode, Result resultCode, Intent data)
+		{
+			base.OnActivityResult(requestCode, resultCode, data);
+			var resultEventArgs = new PreferenceManager.ActivityResultEventArgs(true, requestCode, resultCode, data);
+			ActivityResult(this, resultEventArgs);
 		}
 	}
 
